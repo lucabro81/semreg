6bc109b4d6dd31cab30a3a5d80f4a985d55e2b52
lucabro81 <luca.brognara81@gmail.com>
2025-03-29 18:24:00 +0100

    feat: main function

---
+++ src/core/index.ts
@@ -0,0 +1,17 @@
+import { RegexComposer, RegexOperator } from "../types";
+
+export function regex(...components: RegexComposer[]): RegExp {
+  let pattern = "";
+
+  for (const component of components) {
+    if (typeof component === 'function') {
+      pattern = (component as RegexOperator)(pattern);
+    } else if (component instanceof RegExp) {
+      pattern += component.source;
+    } else {
+      pattern += component;
+    }
+  }
+
+  return new RegExp(pattern);
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { QuantificationOptions } from "./quantification-options";
+import { RegexComposer } from "./regexp-composer";
+import { RegexOperator } from "./regexp-operator";
+
+export type { QuantificationOptions, RegexComposer, RegexOperator };
